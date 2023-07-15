0c2f24ff52f790a98ae653964293d8585c8d80f9
Dipak Gupta <dipakg286@gmail.com>
2023-07-15 19:55:57 +0530

    project setup

---
+++ product-management-service/src/main/java/com/apnidukan/ProductManagementService.java
@@ -0,0 +1,11 @@
+package com.apnidukan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagementService {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagementService.class);
+    }
+}
