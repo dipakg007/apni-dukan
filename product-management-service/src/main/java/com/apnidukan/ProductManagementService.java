6718bc72562981cd3685ebcca7841c8b3b4e41b2
Dipak Gupta <dipakg286@gmail.com>
2023-12-09 18:44:10 +0530

    Fixes for component scan

--- product-management-service/src/main/java/com/apnidukan/ProductManagementService.java
+++ product-management-service/src/main/java/com/apnidukan/ProductManagementService.java
@@ -2,8 +2,10 @@ package com.apnidukan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.apnidukan.mapper")
 public class ProductManagementService {
     public static void main(String[] args) {
         SpringApplication.run(ProductManagementService.class);
